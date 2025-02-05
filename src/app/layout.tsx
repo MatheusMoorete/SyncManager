73d26e96709551afa700e9b1a8daf2ba14612c1f
Matheus Morete Espinoso <matheus.moorete@gmail.com>
2025-02-05 15:26:27 -0300

    Alterando fontes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ 
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
   title: "Brow Studio",
@@ -19,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR" className={inter.variable}>
       <body className={inter.className}>{children}</body>
     </html>
   )
